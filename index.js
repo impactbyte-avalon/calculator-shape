98c410ad969c24ec5cd3c547ca8e29d3761516b6
Huda Ridwan <kerupuk.lovers@gmail.com>
2017-10-06 09:39:56 +0700

    Finish All Tasks

---
+++ index.js
@@ -0,0 +1,37 @@
+function calculateSquareArea(a) {
+	return a * a
+}
+
+function calculateSquarePerimeter(a) {
+	return (a + a)* 2
+}
+
+function calculateCircleArea(a) {
+	// body...
+	return (a * a) * Math.PI
+}
+
+function calculateCircleCircumference(a, b) {
+	// body...
+	return Math.PI * b
+}
+
+function calculateCubeArea(a) {
+	// body...
+	return a * a 
+}
+
+function calculateCubeVolume(a) {
+	// body...
+	return a * a * a
+}
+
+function calculateTubeArea(a, b) {
+	// body...
+	return 2 * Math.PI * a * b
+}
+
+function calculateTubeVolume(a, b) {
+	// body...
+	return Math.PI * ((a / 2) ^ 2 ) * b
+}
